e290a8b6cfb5c63d3ddec8c1f1a6c8b0e71f4e39
watermelon <fekihfarouk88@gmail.com>
2024-11-21 18:09:36 +0100

    Springboot Project created

---
+++ goatcrypter/src/test/java/dev/watermelon/goatcrypter/GoatcrypterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.watermelon.goatcrypter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoatcrypterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
