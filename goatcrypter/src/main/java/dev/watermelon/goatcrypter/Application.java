8fcfdcdc47311f40e52caff71062d6d8c0d0a758
watermelon <fekihfarouk88@gmail.com>
2024-11-23 14:16:43 +0100

    Add H2DB

--- goatcrypter/src/main/java/dev/watermelon/goatcrypter/Application.java
+++ goatcrypter/src/main/java/dev/watermelon/goatcrypter/Application.java
@@ -1,14 +1,7 @@
 package dev.watermelon.goatcrypter;
 
-import java.time.LocalDateTime;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import dev.watermelon.goatcrypter.word.Encryption;
-import dev.watermelon.goatcrypter.word.Word;
 
 @SpringBootApplication
 public class Application {
@@ -17,11 +10,11 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
-	CommandLineRunner history(){
-		return args -> {
-			Word word = new Word(1,1, "Farouk", "G.O.A.T", ".IZW]S", Encryption.ENCRYPT, LocalDateTime.now());
-			System.out.println(word);
-		};
-	}
+	// @Bean
+	// CommandLineRunner history(WordRepository wordRepository){
+	// 	return args -> {
+	// 		Word word = new Word(1,1, "Farouk", "G.O.A.T", ".IZW]S", Encryption.ENCRYPT, LocalDateTime.now());
+	// 		wordRepository.create(word);
+	// 	};
+	// }
 }
